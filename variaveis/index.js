7774aa406d3455f3ff69fbf8b698934b9f971b01
Thiago Ferreira <43960067+mxlock@users.noreply.github.com>
2022-10-04 20:13:33 -0300

    primeira aula

---
+++ variaveis/index.js
@@ -0,0 +1,18 @@
+// Declarar uma variável com seu nome
+var nome = "Thiago";
+
+//Declarar uma variável com a sua idade
+var idade = 25;
+
+//Declarar uma variável com sua comida favorita
+var comidaFavorita;
+
+//Atribuir valor a sua comida favorita
+comidaFavorita = "Pizza";
+
+//Declarar 5 variáveis diferentes sem valores
+var carro, marcaCarro, local, tenis, sexo;
+
+
+//Mostre no console todas as variaveis que você criou
+console.log(nome, idade, comidaFavorita, carro, marcaCarro, local, tenis , sexo);
