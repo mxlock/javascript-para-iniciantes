da876e102196181a32e0c6a020ed8c03379afb03
Thiago Ferreira <43960067+mxlock@users.noreply.github.com>
2022-10-04 20:18:59 -0300

    tipos de dados

---
+++ tiposDeDados/index.js
@@ -0,0 +1,23 @@
+// Declare uma variável contendo uma string
+var nome = "Thiago";
+
+// Declare uma variável contendo um número dentro de uma string
+var variavelNum = "Thiago26";
+
+
+// Declare uma variável com a sua idade
+var idade = 25;
+
+
+// Declare duas variáveis, uma com seu nome
+// e outra com seu sobrenome e some as mesmas
+var nome = "Thiago";
+var idade = 25;
+console.log(nome + ' ' + idade + ' anos');
+
+
+// Coloque a seguinte frase em uma variável: It's time
+var frase = "It's time";
+
+// Verifique o tipo da variável que contém o seu nome
+console.log(typeof(frase));
